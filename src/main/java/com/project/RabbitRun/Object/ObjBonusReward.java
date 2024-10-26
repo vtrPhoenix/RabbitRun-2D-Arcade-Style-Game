c96818fafa634c736e13e262c3804f1efaf8a04d
Vansh Thakur Rana <vtr@sfu.ca>
2024-10-25 18:05:27 -0700

    Added bonus reward object class

--- src/main/java/com/project/RabbitRun/Object/ObjBonusReward.java
+++ src/main/java/com/project/RabbitRun/Object/ObjBonusReward.java
@@ -1,4 +1,17 @@
 package com.project.RabbitRun.Object;
 
-public class ObjBonusReward {
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ObjBonusReward extends SuperObject{
+
+    public ObjBonusReward() {
+        name = "Carrot";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Reward/carrot.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
