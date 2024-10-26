a8266ead32276b837e05217f5024030995f5fe4b
Vansh Thakur Rana <vtr@sfu.ca>
2024-10-25 18:03:59 -0700

    Object classes added

--- src/main/java/com/project/RabbitRun/Object/SuperObject.java
+++ src/main/java/com/project/RabbitRun/Object/SuperObject.java
@@ -1,4 +1,21 @@
 package com.project.RabbitRun.Object;
 
+import com.project.RabbitRun.main.GamePanel;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class SuperObject {
+    public BufferedImage image;
+    public String name;
+    public boolean collision = false;
+    public int worldX, worldY;
+
+    public void draw (Graphics2D g2, GamePanel gamePanel) {
+
+        int screenX = worldX;
+        int screenY = worldY;
+
+        g2.drawImage(image, screenX, screenY, gamePanel.tileSize, gamePanel.tileSize, null);
+    }
 }
