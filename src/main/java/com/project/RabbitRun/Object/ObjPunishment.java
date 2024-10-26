0423ad46aa0cd50e21418b0ec504f564ee14402b
Vansh Thakur Rana <vtr@sfu.ca>
2024-10-25 18:06:14 -0700

    Added punishment object class

--- src/main/java/com/project/RabbitRun/Object/ObjPunishment.java
+++ src/main/java/com/project/RabbitRun/Object/ObjPunishment.java
@@ -1,4 +1,17 @@
 package com.project.RabbitRun.Object;
 
-public class ObjPunishment {
+import javax.imageio.ImageIO;
+import java.io.IOException;
+import java.util.Objects;
+
+public class ObjPunishment extends SuperObject{
+
+    public ObjPunishment() {
+        name = "Mushroom";
+        try {
+            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/Punishment/mushroom.png")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
