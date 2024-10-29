74de04419acc0a269955ae5db03f7ef445cfd802
Vansh Thakur Rana <vtr@sfu.ca>
2024-10-29 02:19:31 -0700

    Updated Asset Setter

--- src/main/java/com/project/RabbitRun/main/AssetSetter.java
+++ src/main/java/com/project/RabbitRun/main/AssetSetter.java
@@ -14,7 +14,7 @@ public class AssetSetter {
 
     public void setObject() {
         gamePanel.object[0] = new ObjReward();
-        gamePanel.object[0].worldX = 5 * gamePanel.tileSize;
+        gamePanel.object[0].worldX = 7 * gamePanel.tileSize;
         gamePanel.object[0].worldY = 4 * gamePanel.tileSize;
 
         gamePanel.object[1] = new ObjReward();
@@ -22,15 +22,15 @@ public class AssetSetter {
         gamePanel.object[1].worldY = 10 * gamePanel.tileSize;
 
         gamePanel.object[2] = new ObjBonusReward();
-        gamePanel.object[2].worldX = 8 * gamePanel.tileSize;
-        gamePanel.object[2].worldY = 2 * gamePanel.tileSize;
+        gamePanel.object[2].worldX = 12 * gamePanel.tileSize;
+        gamePanel.object[2].worldY = 3 * gamePanel.tileSize;
 
         gamePanel.object[3] = new ObjBonusReward();
         gamePanel.object[3].worldX = 4 * gamePanel.tileSize;
-        gamePanel.object[3].worldY = 10 * gamePanel.tileSize;
+        gamePanel.object[3].worldY = 15 * gamePanel.tileSize;
 
         gamePanel.object[4] = new ObjPunishment();
-        gamePanel.object[4].worldX = 7 * gamePanel.tileSize;
-        gamePanel.object[4].worldY = 6 * gamePanel.tileSize;
+        gamePanel.object[4].worldX = 9 * gamePanel.tileSize;
+        gamePanel.object[4].worldY = 11 * gamePanel.tileSize;
     }
 }
