959acbd8256ce9adf8d4cf59cbe4ddec90bede6f
Vansh Thakur Rana <vtr@sfu.ca>
2024-11-04 00:14:23 -0800

    Updated AssetSetter with comments.

--- src/main/java/com/project/RabbitRun/main/AssetSetter.java
+++ src/main/java/com/project/RabbitRun/main/AssetSetter.java
@@ -5,14 +5,29 @@ import com.project.RabbitRun.Object.ObjExitDoor;
 import com.project.RabbitRun.Object.ObjPunishment;
 import com.project.RabbitRun.Object.ObjReward;
 
+/**
+ * Responsible for initializing and placing game objects on the game board
+ * in the "Rabbit Run" game. This includes rewards, bonus rewards, punishments,
+ * and the exit door.
+ */
 public class AssetSetter {
-
+    /** Reference to the main game panel, which contains the game state and objects. */
     GamePanel gamePanel;
 
+    /**
+     * Constructs an {@code AssetSetter} with a reference to the {@code GamePanel}.
+     *
+     * @param gamePanel the game panel to which objects will be added
+     */
     public AssetSetter(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
 
+    /**
+     * Places various game objects at specific coordinates on the game board.
+     * Objects include rewards, bonus rewards, and punishments, each assigned
+     * a specific location to appear in the game.
+     */
     public void setObject() {
         gamePanel.object[0] = new ObjReward();
         gamePanel.object[0].worldX = 17 * gamePanel.tileSize;
@@ -23,8 +38,6 @@ public class AssetSetter {
         gamePanel.object[1].worldY = 16 * gamePanel.tileSize;
 
         gamePanel.object[2] = new ObjBonusReward();
-//        gamePanel.object[2].worldX = 24 * gamePanel.tileSize;
-//        gamePanel.object[2].worldY = 6 * gamePanel.tileSize;
 
         gamePanel.object[3] = new ObjReward();
         gamePanel.object[3].worldX = 14 * gamePanel.tileSize;
@@ -39,8 +52,6 @@ public class AssetSetter {
         gamePanel.object[6].worldY = 22 * gamePanel.tileSize;
 
         gamePanel.object[7] = new ObjBonusReward();
-//        gamePanel.object[7].worldX = 11 * gamePanel.tileSize;
-//        gamePanel.object[7].worldY = 33 * gamePanel.tileSize;
 
         gamePanel.object[8] = new ObjReward();
         gamePanel.object[8].worldX = 29 * gamePanel.tileSize;
@@ -55,8 +66,6 @@ public class AssetSetter {
         gamePanel.object[10].worldY = 20 * gamePanel.tileSize;
 
         gamePanel.object[11] = new ObjBonusReward();
-//        gamePanel.object[11].worldX = 38 * gamePanel.tileSize;
-//        gamePanel.object[11].worldY = 8 * gamePanel.tileSize;
 
         gamePanel.object[12] = new ObjReward();
         gamePanel.object[12].worldX = 21 * gamePanel.tileSize;
@@ -71,6 +80,5 @@ public class AssetSetter {
         gamePanel.object[14].worldY = 8 * gamePanel.tileSize;
 
         gamePanel.object[15] = new ObjBonusReward();
-
     }
 }
