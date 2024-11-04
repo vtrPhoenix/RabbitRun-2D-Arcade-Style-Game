c057a75bfdf9fe80615fa681ce9287b8ed599008
Vansh Thakur Rana <vtr@sfu.ca>
2024-11-04 00:13:14 -0800

    Updated Reward with comments.

--- src/main/java/com/project/RabbitRun/Object/ObjReward.java
+++ src/main/java/com/project/RabbitRun/Object/ObjReward.java
@@ -4,8 +4,16 @@ import javax.imageio.ImageIO;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * Represents a regular reward object in the "Rabbit Run" game.
+ * This object adds points to the player's score when collected.
+ */
 public class ObjReward extends SuperObject{
 
+    /**
+     * Initializes a reward object with a "Clover" image.
+     * This reward contributes to the player's score upon collection.
+     */
     public ObjReward() {
         name = "Clover";
         try {
