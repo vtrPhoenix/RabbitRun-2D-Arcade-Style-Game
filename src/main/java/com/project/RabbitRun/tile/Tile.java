5c2c1f2faa951d0c33ec25605ba4045ddf598df8
Vansh Thakur Rana <vtr@sfu.ca>
2024-11-04 13:14:47 -0800

    Updated Tile class with comments

--- src/main/java/com/project/RabbitRun/tile/Tile.java
+++ src/main/java/com/project/RabbitRun/tile/Tile.java
@@ -2,13 +2,21 @@ package com.project.RabbitRun.tile;
 
 import java.awt.image.BufferedImage;
 
+/**
+ * Represents a tile in the "Rabbit Run" game.
+ * Each tile can have an image and a collision property
+ * indicating if it can be passed through or not.
+ */
 public class Tile {
     /**
-     * contains buffered image of the tile
+     * The image representing the visual appearance of the tile.
      */
     public BufferedImage image;
+
     /**
-     * initially sets the value of collision for the tile to false
+     * Determines whether the tile has collision properties.
+     * {@code true} if the tile is solid and cannot be traversed;
+     * {@code false} otherwise. Default is {@code false}.
      */
     public boolean collision = false;
 }
