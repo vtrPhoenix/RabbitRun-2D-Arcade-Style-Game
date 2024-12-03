ac6bc0777910fca1e095e50df8c1ec9ac32d9ea9
Vansh Thakur Rana <vtr@sfu.ca>
2024-12-03 11:05:09 -0800

    Updated Bonus Rewards to refactor

--- src/main/java/com/project/RabbitRun/object/ObjBonusReward.java
+++ src/main/java/com/project/RabbitRun/object/ObjBonusReward.java
@@ -1,11 +1,5 @@
 package com.project.RabbitRun.object;
 
-import com.project.RabbitRun.exceptions.ImageLoadingException;
-
-import javax.imageio.ImageIO;
-import java.io.IOException;
-import java.util.Objects;
-
 /**
  * Represents a bonus reward object in the "Rabbit Run" game.
  * This object provides extra points to the player when collected.
@@ -17,14 +11,6 @@ public class ObjBonusReward extends SuperObject{
      * This reward grants additional points to the player upon collection.
      */
     public ObjBonusReward() {
-        name = "Carrot";
-        path = "/Reward/carrot.png";
-        try {
-            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path)));
-        }  catch (NullPointerException e) {
-            throw new ImageLoadingException("Image not found: " + path, e, path);
-        } catch (IOException e) {
-            throw new ImageLoadingException("Failed to load image: " + path, e, path);
-        }
+        super("Carrot", "/Reward/carrot.png");
     }
 }
