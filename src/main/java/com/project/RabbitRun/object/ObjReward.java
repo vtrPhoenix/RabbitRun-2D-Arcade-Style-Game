d095b59ae2f31bdf261434c6cff607f8d8bc27f5
Vansh Thakur Rana <vtr@sfu.ca>
2024-12-03 11:04:48 -0800

    Updated Rewards to refactor

--- src/main/java/com/project/RabbitRun/object/ObjReward.java
+++ src/main/java/com/project/RabbitRun/object/ObjReward.java
@@ -1,11 +1,5 @@
 package com.project.RabbitRun.object;
 
-import com.project.RabbitRun.exceptions.ImageLoadingException;
-
-import javax.imageio.ImageIO;
-import java.io.IOException;
-import java.util.Objects;
-
 /**
  * Represents a regular reward object in the "Rabbit Run" game.
  * This object adds points to the player's score when collected.
@@ -17,14 +11,6 @@ public class ObjReward extends SuperObject{
      * This reward contributes to the player's score upon collection.
      */
     public ObjReward() {
-        name = "Clover";
-        path = "/Reward/clover.png";
-        try {
-            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path)));
-        }  catch (NullPointerException e) {
-            throw new ImageLoadingException("Image not found: " + path, e, path);
-        } catch (IOException e) {
-            throw new ImageLoadingException("Failed to load image: " + path, e, path);
-        }
+        super("Clover", "/Reward/clover.png");
     }
 }
