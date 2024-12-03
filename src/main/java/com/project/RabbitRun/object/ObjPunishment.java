49f560f3e49214e7fcec522f8183a30616f1c428
Vansh Thakur Rana <vtr@sfu.ca>
2024-12-03 11:05:55 -0800

    Updated Punishment class to refactor

--- src/main/java/com/project/RabbitRun/object/ObjPunishment.java
+++ src/main/java/com/project/RabbitRun/object/ObjPunishment.java
@@ -1,11 +1,5 @@
 package com.project.RabbitRun.object;
 
-import com.project.RabbitRun.exceptions.ImageLoadingException;
-
-import javax.imageio.ImageIO;
-import java.io.IOException;
-import java.util.Objects;
-
 /**
  * Represents a punishment object in the "Rabbit Run" game.
  * This object, when encountered by the player, may reduce the player's score.
@@ -17,14 +11,6 @@ public class ObjPunishment extends SuperObject{
      * This object is associated with a penalty when the player interacts with it.
      */
     public ObjPunishment() {
-        name = "Mushroom";
-        path = "/Punishment/mushroom.png";
-        try {
-            image = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream(path)));
-        }  catch (NullPointerException e) {
-            throw new ImageLoadingException("Image not found: " + path, e, path);
-        } catch (IOException e) {
-            throw new ImageLoadingException("Failed to load image: " + path, e, path);
-        }
+        super("Mushroom", "/Punishment/mushroom.png");
     }
 }
